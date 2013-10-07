af7c149b21a8e5d973bfe8e3abba55030e8decb5
idsc3101 <idsc3101@gmail.com>
2013-10-07 15:19:33 -0500

    In class work_Jana

--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,26 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+        Dim x As Double = CDbl(txtSideOne.Text)
+        Dim y As Double = CDbl(txtSideTwo.Text)
+        Dim z As Double
 
-        txtHyp.Text = "Not Implemented Yet"
+        'this is the function
+        z = Hypotenuse(x,y)
+
+        txtHyp.Text = CStr(z)
 
     End Sub
 
+    'ByRef would change the original, so we use ByVal
+    'MUST INCLUDE "RETURN"
+    Function Hypotenuse(ByVal a As Double, b As Double) As Double
+        Dim c As Double
+
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+
+        Return c
+    End Function
+
 End Class
+
